02810f46fdb476fb4b5fcb56895299fa9130d7a1
SkySoHigh <SkySoHigh39@gmail.com>
2022-05-12 01:06:21 +0300

    Added debug params to config and corresponding options to engine

--- api/client/db.py
+++ api/client/db.py
@@ -1,21 +1,28 @@
-from api.controllers.db import UserDBController
+from config import ECHO_DB_QUERIES, ECHO_CONNECTION_POOL
 from api.transport.db import DBSession
+from api.controllers.db import UserExampleDBController
 
 
 class DBClient:
-    def __init__(self, url: str, pool_size: int = 5, max_overflow: int = 5):
+    def __init__(self, url: str, pool_size: int = 5, max_overflow: int = 5, echo: bool = ECHO_DB_QUERIES,
+                 echo_pool: bool = ECHO_CONNECTION_POOL):
         """Database client, which provides access for all db controllers and methods.
 
         Args:
             url (str): Database connection string (DSN). Example format: {DRIVER}://{USER}:{PASSWORD}@{HOST}:{PORT}/{SID}
             pool_size (int, optional): Number of connections to be maintained. Defaults to 5.
             max_overflow (int, optional): The maximum overflow size of the pool. Defaults to 5.
+            echo (boolean, optional): if True, the Engine will log all statements.
+            echo_pool (boolean, optional): if True, the connection pool will log informational output such as
+            when connections are invalidated as well as when connections are recycled.
         """
 
         self.__db_session = DBSession(url=url,
                                       pool_size=pool_size,
-                                      max_overflow=max_overflow)
-        self.users = UserDBController(self.session)
+                                      max_overflow=max_overflow,
+                                      echo=echo,
+                                      echo_pool=echo_pool)
+        self.users = UserExampleDBController(self.session)
 
     @property
     def session(self):
--- api/transport/db.py
+++ api/transport/db.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session, Query
 from sqlalchemy.pool import QueuePool
@@ -12,18 +11,24 @@ def refresh_info_in_session(query):
 
 class DBSession:
 
-    def __init__(self, url: str, pool_size: int = 5, max_overflow: int = 5):
+    def __init__(self, url: str, pool_size: int = 5, max_overflow: int = 5, echo: bool = False,
+                 echo_pool: bool = False):
         """Database session constructor.
 
         Args:
             url (str): Database connection string (DSN). Example format: {DRIVER}://{USER}:{PASSWORD}@{HOST}:{PORT}/{SID}
-            pool_size (int, optional): Number of connections to be maintained. Defaults to 5.
-            max_overflow (int, optional): The maximum overflow size of the pool. Defaults to 5.
+            pool_size (int, optional): Number of connections to be maintained. Defaults: 5.
+            max_overflow (int, optional): The maximum overflow size of the pool. Defaults: 5.
+            echo (boolean, optional): if True, the Engine will log all statements. Defaults: False
+            echo_pool (boolean, optional): if True, the connection pool will log informational output such as
+            when connections are invalidated as well as when connections are recycled. Defaults: False
         """
         self.__engine = create_engine(url,
                                       poolclass=QueuePool,
                                       pool_size=pool_size,
-                                      max_overflow=max_overflow)
+                                      max_overflow=max_overflow,
+                                      echo=echo,
+                                      echo_pool=echo_pool)
         self.__session = Session(self.__engine, future=True)
 
     @property
