0dfddf79df3a4d92cb7c51aec1e33b4476bc005b
SkySoHigh <SkySoHigh39@gmail.com>
2022-08-19 12:49:35 +0300

    Added http config initialization based on env variables

--- configs/http.py
+++ configs/http.py
@@ -1,15 +1,19 @@
+import json
+import os
+
 
 class HttpConfig:
-    PROTOCOL: str = 'http'
-    HOST: str = '192.168.126.248'
-    PORT: int = 8083
-    BASE_ENDPOINT: str = ''
+    PROTOCOL: str = os.environ.get('http_protocol', 'http')
+    HOST: str = os.environ.get('http_host', '')
+    PORT: int = os.environ.get('http_port', '')
+    BASE_ENDPOINT: str = os.environ.get('http_base_endpoint', '/')
 
-    USERNAME: str = 'admin'
-    PASSWORD: str = 'elephant'
+    USERNAME: str = os.environ.get('http_username', 'username')
+    PASSWORD: str = os.environ.get('http_password', 'password')
 
     URL: str = f'{PROTOCOL}://{HOST}:{PORT}/{BASE_ENDPOINT}'
-    DEFAULT_HEADERS: dict = {}
-    DEFAULT_COOKIES: dict = {}
 
-    FOLLOW_REDIRECTS: bool = True
+    DEFAULT_HEADERS: dict = json.loads(os.environ.get('http_headers', '').replace("'", "\""))
+    DEFAULT_COOKIES: dict = json.loads(os.environ.get('http_cookies', '').replace("'", "\""))
+
+    FOLLOW_REDIRECTS: bool = bool(int(os.environ.get('http_follow_redirects', True)))
