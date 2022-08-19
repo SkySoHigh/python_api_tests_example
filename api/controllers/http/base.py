4a4bd13b2cef74a4d0ef655d61bc4477a9ee5bb0
SkySoHigh <SkySoHigh39@gmail.com>
2022-08-19 13:55:03 +0300

    Added endpoint to BaseHttpController

--- api/controllers/http/base.py
+++ api/controllers/http/base.py
@@ -12,10 +12,12 @@ class BaseHttpController(Generic[HTTP_MODEL_TYPE]):
     Contains a basic set of private methods for performing operations with http objects.
     """
 
-    def __init__(self, transport: HttpTransport):
+    def __init__(self, transport: HttpTransport, endpoint: str):
         """
         Args:
             transport: HttpTransport object for executing commands in the database (transport layer).
+            endpoint: http endpoint (to be added to base_url)
         """
-        self.__model = get_generic_type_arg(self)[0]
-        self.__http_client = transport
+        self.model = get_generic_type_arg(self)[0]
+        self.transport = transport
+        self.endpoint = endpoint
