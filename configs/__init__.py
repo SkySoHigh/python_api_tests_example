9c8ad65926d2e4614785a311b136394694171e05
SkySoHigh <SkySoHigh39@gmail.com>
2022-08-19 13:36:38 +0300

    Added dotenv load before configs initialization

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+
+# Essential to load env variables from .env file before loading config classes based on those variables
+load_dotenv()
+
 from .db import DbConfig
 from .http import HttpConfig
 from .common import CommonConfig
